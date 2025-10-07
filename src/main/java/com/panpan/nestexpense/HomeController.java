29ba2577700c007df81165885c2c4736f74a9e13
panpan <nandhawanwongkuprasert@gmail.com>
2025-10-07 18:42:26 +0700

    create home page

---
+++ src/main/java/com/panpan/nestexpense/HomeController.java
@@ -0,0 +1,13 @@
+package com.panpan.nestexpense;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
