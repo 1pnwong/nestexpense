7a9a127593605dfcff83aebc9b1fd60ba530ba58
panpan <nandhawanwongkuprasert@gmail.com>
2025-10-07 18:06:26 +0700

    initialised project

---
+++ src/main/java/com/panpan/nestexpense/NestexpenseApplication.java
@@ -0,0 +1,13 @@
+package com.panpan.nestexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestexpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestexpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panpan/nestexpense/NestexpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panpan.nestexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestexpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
